e2433b0b81edbd13c28ff90983eefefa55dbbbad
MiaoDragon <innocentmyl@gmail.com>
2019-07-17 14:12:05 -0700

    set up training for baxter

--- pytorch/models/common.py
+++ pytorch/models/common.py
@@ -3,78 +3,34 @@ import torch.nn as nn
 import torch.nn.functional as F
 from torch.autograd import Variable
 import numpy as np
-
-class STN3d(nn.Module):
-    def __init__(self, args, num_points=2500):
-        super(STN3d, self).__init__()
-        self.num_points = num_points
-        self.args = args
-        self.conv1 = torch.nn.Conv1d(3, 64, 1)
-        self.conv2 = torch.nn.Conv1d(64, 128, 1)
-        self.conv3 = torch.nn.Conv1d(128, 1024, 1)
-        self.fc1 = nn.Linear(1024, 512)
-        self.fc2 = nn.Linear(512, 256)
-        self.fc3 = nn.Linear(256, 9)
-        self.relu = nn.ReLU()
-
-    def forward(self, x):
-        batchsize = x.size()[0]
-        x = F.relu(self.conv1(x))
-        x = F.relu(self.conv2(x))
-        x = F.relu(self.conv3(x))
-        x,_ = torch.max(x, 2)
-        x = x.view(-1, 1024)
-
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        x = self.fc3(x)
-
-        iden = Variable(torch.from_numpy(np.array([1,0,0,0,1,0,0,0,1]).astype(np.float32))).view(1,9).repeat(batchsize,1)
-        if x.is_cuda:
-            iden = iden.cuda()
-        x = x + iden
-        x = x.view(-1, 3, 3)
-        return x
-
 class PointNetfeat(nn.Module):
-    def __init__(self, args, num_points=2500, global_feat=True, trans=False):
+    def __init__(self, code_ntfs, num_points=2500, global_feat=True, trans=False):
         super(PointNetfeat, self).__init__()
-        self.args = args
-        #self.stn = STN3d(args, num_points=num_points)
-        self.conv1 = torch.nn.Conv1d(3, 64, 1)
-        self.conv2 = torch.nn.Conv1d(64, 128, 1)
-        self.conv3 = torch.nn.Conv1d(128, args.code_nfts, 1)
-
-        self.bn1 = torch.nn.BatchNorm1d(64)
-        self.bn2 = torch.nn.BatchNorm1d(128)
-        self.bn3 = torch.nn.BatchNorm1d(args.code_nfts)
-        self.trans = trans
-
-        self.num_points = num_points
-        self.global_feat = global_feat
+        self.ntfs = code_ntfs
+        #self.stn = STN3d(self, num_points=num_points)
+        self.convs = [  torch.nn.Conv1d(3, 64, 1),
+                        torch.nn.Conv1d(64, 128, 1),
+                        torch.nn.Conv1d(128, 128, 1),
+                        torch.nn.Conv1d(128, 256, 1),
+                        torch.nn.Conv1d(256, self.code_nfts, 1)]
+        self.bns = [
+                        torch.nn.BatchNorm1d(64),
+                        torch.nn.BatchNorm1d(128),
+                        torch.nn.BatchNorm1d(128),
+                        torch.nn.BatchNorm1d(256),
+                        torch.nn.BatchNorm1d(self.code_nfts)]
+        self.dropout = torch.nn.Dropout()
+        self.relu = torch.nn.ReLU()
 
     def forward(self, x):
-        if self.trans:
-            trans = self.stn(x)
-            x = x.transpose(2,1)
-            x = torch.bmm(x, trans)
-            x = x.transpose(2,1)
-        x = F.relu(self.bn1(self.conv1(x)))
-        pointfeat = x
-        x = F.relu(self.bn2(self.conv2(x)))
-        x = self.bn3(self.conv3(x))
+        for i in range(len(self.convs)):
+            x = self.convs[i](x)
+            x = self.bns[i](x)
+            x = self.relu(x)
+            x = self.dropout(x)
         x,_ = torch.max(x, 2)
-        x = x.view(-1, self.args.code_nfts)
-        if self.trans:
-            if self.global_feat:
-                return x, trans
-            else:
-                x = x.view(-1, self.args.code_nfts, 1).repeat(1, 1, self.num_points)
-                return torch.cat([x, pointfeat], 1), trans
-        else:
-            return x
-
-
+        x = x.view(-1, self.self.code_nfts)
+        return x
 def weights_init(m):
     classname = m.__class__.__name__
     if classname.find('Conv') != -1:
---
+++ pytorch/train.py
@@ -0,0 +1,140 @@
+"""
+"""
+from builtins import range
+import os
+import sys
+sys.path.append(os.getcwd())
+import _init_paths
+from PointNetFCAE import *
+#from modules.emd import EMDModule
+from utils.chamfer.dist_chamfer import chamferDist as chamfer
+from tools.obs_data_loader import load_dataset
+from tools.import_tool import fileImport
+import argparse
+import torch
+import torch.nn as nn
+import numpy as np
+import os
+import pickle
+#from tools.path_data_loader import load_dataset_end2end
+from torch.autograd import Variable
+import time
+
+def to_var(x, volatile=False):
+    if torch.cuda.is_available():
+        x = x.cuda()
+    return Variable(x, volatile=volatile)
+def main(args):
+    os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"   # see issue #152
+    os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+
+    importer = fileImport()
+    env_data_path = args.env_data_path
+    path_data_path = args.path_data_path
+    pcd_data_path = args.pointcloud_data_path
+    # append all envs and obstacles
+    #envs_files = os.listdir(env_data_path)
+    #envs_files = ['trainEnvironments.pkl']
+    envs_files = ['trainEnvironmentsLarge.pkl']
+    #envs_files = ['trainEnvironments.pkl']
+    obstacles = []
+    for envs_file in envs_files:
+        envs = importer.environments_import(env_data_path + envs_file)
+
+        print("Loading obstacle data...\n")
+        obs = load_dataset(envs, pcd_data_path, importer)
+        obstacles.append(obs)
+
+
+    obstacles = np.stack(obstacles).astype(float)[0].reshape(len(obs),-1,3)
+    print(obstacles.shape)
+    print("Loaded dataset, targets, and pontcloud obstacle vectors: ")
+    print("\n")
+
+    if not os.path.exists(args.trained_model_path):
+        os.makedirs(args.trained_model_path)
+
+    # Build the models
+    net = PointNetFCAE(code_ntfs=1024, num_points=len(obstacles[0]), output_channels=3)
+    if torch.cuda.is_available():
+        net.cuda()
+
+
+    # Loss and Optimizer
+    params = list(net.parameters())
+    optimizer = torch.optim.Adam(params, lr=args.learning_rate)
+
+    total_loss = []
+    epoch = 1
+
+    sm = 100  # start saving models after 100 epochs
+
+    print("Starting epochs...\n")
+    # epoch=1
+    done = False
+    for epoch in range(args.num_epochs):
+        # while (not done)
+        # every time use a new obstacle
+        start = time.time()
+        print("epoch" + str(epoch))
+        avg_loss = 0
+        for i in range(0, len(obstacles), args.batch_size):
+            # Forward, Backward and Optimize
+            # zero gradients
+            net.zero_grad()
+            # convert to pytorch tensors and Varialbes
+            bobs = torch.from_numpy(obstacles[i:i+args.batch_size]).type(torch.FloatTensor)
+            #bobs = to_var(bobs).view(len(bobs), -1, 3).permute(0,2,1)
+            bobs = to_var(bobs)
+            # forward pass through encoder
+            bt = net(bobs)
+            # compute overall loss and backprop all the way
+            loss1, loss2 = chamfer()(bobs, bt)
+            #loss1, loss2 = criterion(bobs, bt)
+            print('loss1')
+            print(loss1)
+            print('loss2')
+            print(loss2)
+            loss = torch.mean(loss1) + torch.mean(loss2)
+            print('loss:')
+            print(loss)
+            avg_loss = avg_loss+loss.data
+            loss.backward()
+            optimizer.step()
+
+        print("--average loss:")
+
+        # Save the models
+        if epoch == sm:
+            print("\nSaving model\n")
+            print("time: " + str(time.time() - start))
+            torch.save(net.state_dict(), os.path.join(
+                args.trained_model_path, 'pointnet_'+str(epoch)+'.pkl'))
+            #if (epoch != 1):
+            sm = sm+100  # save model after every 50 epochs from 100 epoch ownwards
+
+    torch.save(total_loss, 'total_loss.dat')
+    print(encoder.state_dict())
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--env_data_path', type=str, default='./env/environment_data/')
+    parser.add_argument('--path_data_path', type=str, default='./data/train/paths/')
+    parser.add_argument('--pointcloud_data_path', type=str, default='./data/train/pcd/')
+    parser.add_argument('--trained_model_path', type=str, default='./models/sample_train/', help='path for saving trained models')
+
+    parser.add_argument('--batch_size', type=int, default=100)
+    parser.add_argument('--learning_rate', type=float, default=0.001)
+    parser.add_argument('--num_epochs', type=int, default=200)
+
+    parser.add_argument('--enc_input_size', type=int, default=16053)
+    parser.add_argument('--enc_output_size', type=int, default=60)
+    parser.add_argument('--mlp_input_size', type=int, default=74)
+    parser.add_argument('--mlp_output_size', type=int, default=7)
+
+    parser.add_argument('--train_ratio', type=float, default=0.8)
+    parser.add_argument('--envs_file', type=str, default='trainEnvironments.pkl')
+    parser.add_argument('--path_data_file', type=str, default='trainPaths.pkl')
+
+    args = parser.parse_args()
+    main(args)
